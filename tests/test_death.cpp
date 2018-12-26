d8c86e391303fdbf40c2f602f7d107a0739e80b5
Chun-Min Chang <chun.m.chang@gmail.com>
2018-12-26 08:55:50 -0800

    Problem: Cannot call a Rust API within a death check twice. The test will stop in the middle.

--- tests/test_death.cpp
+++ tests/test_death.cpp
@@ -18,6 +18,17 @@ TEST(gtest_rust, c_death) {
     ASSERT_DEATH(bang(), "");
 }
 
+// It's ok to call `bang()` twice.
+TEST(gtest_rust, c_death_again) {
+    ASSERT_DEATH(bang(), "");
+}
+
 TEST(gtest_rust, rust_death) {
+    ASSERT_ANY_THROW(boom());
+}
+
+// This test will stop in the middle
+// if `boom()` is called twice.
+TEST(gtest_rust, rust_death_again) {
     ASSERT_ANY_THROW(boom());
 }

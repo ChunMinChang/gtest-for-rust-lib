53d560e37262ac63ad6496ca4a4ffed86afcd921
Chun-Min Chang <chun.m.chang@gmail.com>
2018-12-26 17:59:30 -0800

    Add a test to check if ASSERT_ANY_THROW can work with C exception

--- tests/test_death.cpp
+++ tests/test_death.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <csignal>
 #include "gtest/gtest.h"
 
 #if defined(__cplusplus)
@@ -14,21 +15,35 @@ extern void boom();
 
 void bang() { assert(false); }
 
+void panic() { throw SIGILL; }
+
+// Hitting a C assertion twice
+// ----------------------------------------------------------------------------
 TEST(gtest_rust, c_death) {
     ASSERT_DEATH(bang(), "");
 }
 
-// It's ok to call `bang()` twice.
 TEST(gtest_rust, c_death_again) {
     ASSERT_DEATH(bang(), "");
 }
 
+// Catching a C exception twice
+// ----------------------------------------------------------------------------
+TEST(gtest_rust, c_throw_an_exception) {
+    ASSERT_ANY_THROW(panic());
+}
+
+TEST(gtest_rust, c_throw_an_exception_again) {
+    ASSERT_ANY_THROW(panic());
+}
+
+// Hitting a Rust assertion twice
+// ----------------------------------------------------------------------------
 TEST(gtest_rust, rust_death) {
     ASSERT_ANY_THROW(boom());
 }
 
-// This test will stop in the middle
-// if `boom()` is called twice.
+// This test will stop in the middle when `boom()` is called again!
 TEST(gtest_rust, rust_death_again) {
     ASSERT_ANY_THROW(boom());
 }
